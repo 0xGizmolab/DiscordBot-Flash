eb94d7db7e73bcd8c9b764c7d8a573ab697b8a02
Abbas Ali <abbasali.mehtar@gmail.com>
2021-08-03 12:58:53 +0530

    Delete sleep.js

--- sleep.js
+++
@@ -1,9 +0,0 @@
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-  
-  module.exports = { sleep }
